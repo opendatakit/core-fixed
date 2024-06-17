706e85b5b88385ccddcf110f90cbdc74be9dc740
Stryker101 <onuoha.henrychika@gmail.com>
2024-06-17 10:56:19 +0100

    using recyclerview idling resource

--- services_app/src/androidTest/java/org/opendatakit/services/preferences/activities/AdminAppPropertiesActivityTest.java
+++ services_app/src/androidTest/java/org/opendatakit/services/preferences/activities/AdminAppPropertiesActivityTest.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -16,16 +17,12 @@ import android.content.Intent;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.IdlingRegistry;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opendatakit.BaseUITest;
-import org.opendatakit.IdlingResource;
 import org.opendatakit.RecyclerViewIdlingResource;
 import org.opendatakit.consts.IntentConsts;
 import org.opendatakit.properties.PropertiesSingleton;
@@ -33,32 +30,28 @@ import org.opendatakit.services.R;
 
 @RunWith(AndroidJUnit4.class)
 public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActivity> {
-
-    @Rule
-    public ActivityScenarioRule<AppPropertiesActivity> activityRule = new ActivityScenarioRule<>(AppPropertiesActivity.class);
+    private RecyclerViewIdlingResource recyclerViewIdlingResource;
 
     @Override
     protected void setUpPostLaunch() {
-        activityRule.getScenario().onActivity(activity -> {
-            IdlingResource.increment();
+        activityScenario.onActivity(activity -> {
             PropertiesSingleton props = activity.getProps();
             assertThat(props).isNotNull();
-            IdlingResource.decrement();
+            RecyclerView recyclerView = activity.findViewById(androidx.preference.R.id.recycler_view);
+            recyclerViewIdlingResource = new RecyclerViewIdlingResource(recyclerView);
+            IdlingRegistry.getInstance().register(recyclerViewIdlingResource);
         });
         enableAdminMode();
         onView(withId(R.id.app_properties_content)).check(matches(isDisplayed()));
 
-        activityRule.getScenario().onActivity(activity -> {
-            RecyclerView recyclerView = activity.findViewById(androidx.preference.R.id.recycler_view);
-            RecyclerViewIdlingResource idlingResource = new RecyclerViewIdlingResource(recyclerView);
-            IdlingRegistry.getInstance().register(idlingResource);
-        });
     }
 
     @Test
     public void checkIfChangeAdminPasswordScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(3, scrollTo()))
-                .check(matches(atPosition(3, hasDescendant(withText(R.string.change_admin_password)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(3))
+                .perform(actionOnItemAtPosition(3, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.change_admin_password))));
         onView(allOf(withId(android.R.id.summary),
                 childAtPosition(withId(androidx.preference.R.id.recycler_view), 3),
                 isDisplayed())).check(matches(withText(R.string.admin_password_enabled)));
@@ -66,8 +59,10 @@ public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActi
 
     @Test
     public void checkIfManageAbilityToChangeServerSettingScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(8, scrollTo()))
-                .check(matches(atPosition(8, hasDescendant(withText(R.string.restrict_server)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(8))
+                .perform(actionOnItemAtPosition(8, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.restrict_server))));
         onView(allOf(withId(android.R.id.summary),
                 childAtPosition(withId(androidx.preference.R.id.recycler_view), 8),
                 isDisplayed())).check(matches(withText(R.string.restrict_server_settings_summary)));
@@ -75,8 +70,10 @@ public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActi
 
     @Test
     public void checkIfManageAbilityToChangeDeviceSettingScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(9, scrollTo()))
-                .check(matches(atPosition(9, hasDescendant(withText(R.string.restrict_device)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(9))
+                .perform(actionOnItemAtPosition(9, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.restrict_device))));
         onView(allOf(withId(android.R.id.summary),
                 childAtPosition(withId(androidx.preference.R.id.recycler_view), 9),
                 isDisplayed())).check(matches(withText(R.string.restrict_device_settings_summary)));
@@ -84,8 +81,10 @@ public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActi
 
     @Test
     public void checkIfManageAbilityToChangeTableSpecificSettingScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(10, scrollTo()))
-                .check(matches(atPosition(10, hasDescendant(withText(R.string.admin_tool_tables_settings)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(10))
+                .perform(actionOnItemAtPosition(10, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.admin_tool_tables_settings))));
         onView(allOf(withId(android.R.id.summary),
                 childAtPosition(withId(androidx.preference.R.id.recycler_view), 10),
                 isDisplayed())).check(matches(withText(R.string.admin_tool_tables_settings_summary)));
@@ -93,8 +92,10 @@ public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActi
 
     @Test
     public void checkIfResetConfigurationScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(6, scrollTo()))
-                .check(matches(atPosition(6, hasDescendant(withText(R.string.clear_settings)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(6))
+                .perform(actionOnItemAtPosition(6, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.clear_settings))));
         onView(allOf(withId(android.R.id.summary),
                 childAtPosition(withId(androidx.preference.R.id.recycler_view), 6),
                 isDisplayed())).check(matches(withText(R.string.clear_configuration_settings)));
@@ -102,14 +103,18 @@ public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActi
 
     @Test
     public void checkIfExitAdminModeScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(11, scrollTo()))
-                .check(matches(atPosition(11, hasDescendant(withText(R.string.exit_admin_mode)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(11))
+                .perform(actionOnItemAtPosition(11, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.exit_admin_mode))));
     }
 
     @Test
     public void checkIfVerifyUserPermissionScreen_isVisible() {
-        onView(withId(androidx.preference.R.id.recycler_view)).perform(actionOnItemAtPosition(2, scrollTo()))
-                .check(matches(atPosition(2, hasDescendant(withText(R.string.verify_server_settings_header)))));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(scrollToPosition(2))
+                .perform(actionOnItemAtPosition(2, scrollTo()))
+                .check(matches(hasDescendant(withText(R.string.verify_server_settings_header))));
         onView(allOf(withId(android.R.id.summary),
                 childAtPosition(withId(androidx.preference.R.id.recycler_view), 2),
                 isDisplayed())).check(matches(withText(R.string.click_to_verify_server_settings)));
@@ -117,7 +122,9 @@ public class AdminAppPropertiesActivityTest extends BaseUITest<AppPropertiesActi
 
     @After
     public void tearDown() {
-        IdlingRegistry.getInstance().unregister(IdlingResource.getIdlingResource());
+        if (recyclerViewIdlingResource != null) {
+            IdlingRegistry.getInstance().unregister(recyclerViewIdlingResource);
+        }
         resetConfiguration();
     }
 
